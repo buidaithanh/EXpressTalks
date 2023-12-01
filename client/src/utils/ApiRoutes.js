638b23d6eec0f40253dccb5918d2d62de756fa5e
dai thanh <thanh0985837774@gmail.com>
2023-12-01 20:46:10 +0700

    change host

--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:6060";
+export const host = "https://server-express-talksss.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setavatar`;
